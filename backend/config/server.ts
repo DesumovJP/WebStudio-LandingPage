4a90c1737124ae98fbfa6d1f61f0084f95e0d227
Олександр Сімченко <oleksandrsimcenko@MacBook-Air-Oleksandr.local>
2025-11-05 01:01:30 +0200

    Update backend server configuration

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
-  url: env('PUBLIC_URL', 'https://your-app-name.up.railway.app'), // заміни на свій домен
+  url: env('PUBLIC_URL', 'https://webstudio-landingpage.up.railway.app'),
+ // заміни на свій домен
   app: {
     keys: env.array('APP_KEYS'),
   },
