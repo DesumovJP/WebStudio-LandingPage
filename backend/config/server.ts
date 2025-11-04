51376c0a4308f61f48869ae2d12db498d8e0e3be
Олександр Сімченко <oleksandrsimcenko@MacBook-Air-Oleksandr.local>
2025-11-05 01:00:41 +0200

    Update project files

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080), // ← ключова зміна
+  port: env.int('PORT', 8080),
+  url: env('PUBLIC_URL', 'https://your-app-name.up.railway.app'), // заміни на свій домен
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
-
