50c56718785e73db4198c3549a992ec3fd0156de
Олександр Сімченко <oleksandrsimcenko@MacBook-Air-Oleksandr.local>
2025-11-08 18:26:31 +0200

    fix: add type assertion to fix TypeScript build errors

--- backend/src/api/project/services/project.ts
+++ backend/src/api/project/services/project.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::project.project');
+export default factories.createCoreService('api::project.project' as any);
