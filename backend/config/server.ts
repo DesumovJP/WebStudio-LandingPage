e8e036d4bae9a537d8248a741d7354cbdf2ef686
Олександр Сімченко <oleksandrsimcenko@MacBook-Air-Oleksandr.local>
2025-11-08 17:45:09 +0200

    Update backend configuration and generated types

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,9 +1,17 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
-  url: env('PUBLIC_URL', 'https://webstudio-landingpage.up.railway.app'),
- // заміни на свій домен
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  const isDevelopment = env('NODE_ENV') !== 'production';
+  
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', isDevelopment ? 1337 : 8080),
+    url: env(
+      'PUBLIC_URL',
+      isDevelopment
+        ? 'http://localhost:1337'
+        : 'https://webstudio-landingpage-production.up.railway.app'
+    ),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
